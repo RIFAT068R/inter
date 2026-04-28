566767c46f1d68caa09e64af3cff8a090be0bdb3
RIFAT068R <rrr782677@gmail.com>
2026-04-28 22:03:52 +0600

    Add subtle global motion polish

--- components/app-shell.tsx
+++ components/app-shell.tsx
@@ -8,7 +8,7 @@ export function AppShell({ children }: { children: React.ReactNode }) {
       <div className="pointer-events-none absolute inset-0 bg-grid opacity-50" />
       <Navbar />
       <div className="relative mx-auto flex min-h-screen w-full max-w-[1200px] flex-col px-4 pb-6 pt-24 sm:px-6 sm:pb-8 sm:pt-28 lg:px-8 lg:pb-10 lg:pt-28">
-        <main className="flex-1">{children}</main>
+        <main className="page-enter flex-1">{children}</main>
       </div>
     </div>
   );
